66249147da0ddd3e004b52474966a9e5f7a1c87e
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-06 10:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+const imgs = document.querySelectorAll(".img");
+
+imgs.forEach(img => {
+	img.addEventListener("click", () => {
+		const border = img.style.border;
+		if(border !== '5px solid red'){
+			img.style.border = '5px solid red';
+		}else{
+			img.style.border = '5px solid gray';
+		}
+	});
+})
+
+
+
